d7af3befa2b20b30ab8b76bd4c16abf8a9093201
Chris Sanders <sanders.chris@gmail.com>
2022-07-21 09:20:46 -0500

    Add taskfile and workflow

--- main.go
+++ main.go
@@ -43,21 +43,23 @@ func main() {
 	for {
 		sc, err := clientset.StorageV1().StorageClasses().List(context.TODO(), metav1.ListOptions{})
 		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("Found  %d storage classes: \n", len(sc.Items))
-		for _, storageClass := range sc.Items {
-			fmt.Println("Annotations:")
-			for name, value := range storageClass.ObjectMeta.Annotations {
-				fmt.Printf("%v:%v \n", name, value)
-				if (name == "storageclass.kubernetes.io/is-default-class") && (value == "true") {
-					fmt.Println("Deafult storage class found, exiting")
-					os.Exit(0)
+			fmt.Printf("Unable to list Storage classes: %v", err.Error())
+			// panic(err.Error())
+		} else {
+			fmt.Printf("Found  %d storage classes: \n", len(sc.Items))
+			for _, storageClass := range sc.Items {
+				fmt.Println("Annotations:")
+				for name, value := range storageClass.ObjectMeta.Annotations {
+					fmt.Printf("%v:%v \n", name, value)
+					if (name == "storageclass.kubernetes.io/is-default-class") && (value == "true") {
+						fmt.Println("Deafult storage class found, exiting")
+						os.Exit(0)
+					}
 				}
 			}
 		}
 
 		fmt.Println("No default storage class found trying again later")
-		time.Sleep(2 * time.Second)
+		time.Sleep(5 * time.Second)
 	}
 }
